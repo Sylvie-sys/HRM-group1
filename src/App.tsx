a22b535271cba493a3bbefdd6ab0bb0a3531a717
Sylvie-sys <mahorosisi@gmail.com>
2022-12-22 14:18:16 +0200

    folders structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Input from './Components/Inputs'
+function App() {
+
+
+  return (
+   <Input /> 
+  )
+}
+
+export default App
---
+++ src/Components/Inputs/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./index.css"
+
+const Input =() =>{
+return(
+
+     <div>
+        <input type="text" placeholder="firstname" />
+        <input type="text" placeholder="lastname" />
+        <br/>
+        <input type="email" placeholder="email" />
+        <input type="number" placeholder="Phonenumber" />
+        <br/>
+        <input type="password" placeholder="password" />
+        <input type="password" placeholder="confirpassword" />
+        <br/>
+        <input type="date" placeholder="dateofbirth" />
+        <input type="" />
+        <input type="image" placeholder="personalpic" />
+     </div>
+)
+
+}
+
+
+export default Input;
