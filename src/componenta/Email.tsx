9428050c074c299e6794a4f153c03c944a9dd6fa
SIMBI-Gloria <simbigloria9@gmail.com>
2023-01-13 14:39:52 +0200

    pushing forgot password page

---
+++ src/componenta/Email.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Email.css'
+
+const EmailForm=() =>{
+    return(
+        <div className="cover">
+         <h1>Forgot Your Password</h1>
+         <p className="no">No worries,we'll send you reset instructions <br />  </p>
+           <p className="thr"> through this email</p>
+          
+         <input type="text" placeholder="Admin@gmail.com" className="new"/>
+         <button className="confirm">confirm</button>
+        </div>
+    )
+}
+export default EmailForm 
