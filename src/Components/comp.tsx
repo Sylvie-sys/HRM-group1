838b3c8cc5bef432c45f83c42bf310a765245567
deborah <niwudeby@gmail.com>
2023-01-16 15:04:06 +0200

    updated true inputs

--- src/Components/comp.tsx
+++ src/Components/comp.tsx
@@ -3,58 +3,63 @@ import "./comp.css"
 import { TextFieldExplained } from "./text-field-explained";
 import TextInp from "./text/text"
 
-const Components =() =>{
+const Components = () => {
 
     let email = "";
     let password = "";
-    let number ="";
+    let number = "";
     const onEmailChange: ChangeEventHandler<HTMLInputElement> = (v) => {
         email = v.target.value;
-        
+
     }
     const onPasswordChange: ChangeEventHandler<HTMLInputElement> = (v) => {
         password = v.target.value;
-        
+
     }
     const onNumberChange: ChangeEventHandler<HTMLInputElement> = (v) => {
         password = v.target.value;
-        
+
     }
 
     const loginAction = (evt: any) => {
         evt.preventDefault();
-        const cred = {email, password, number};
+        const cred = { email, password, number };
         // call the auth api
         console.log(cred);
-        
+
     }
 
-return (
-<div className="create">
-<div className='header-text'>
-            <h3>Create Account</h3>
+    return (
+        <div className="create">
 
-    <form action="" onSubmit={loginAction} className="form">
-        <div className="input-field">
-        <TextFieldExplained  name="name" type="text" variant="two" label="FirstName" />
-        <TextFieldExplained  name="name" type="text" variant="two" label="LaststName" /> 
+            <div className="container">
+
+
+                <form action="" onSubmit={loginAction} className="form">
+                    <div className='header-text'>
+                        <h3>Create Account</h3>
+                    </div>
+                    <div className="input-field1">
+                        <TextFieldExplained name="name" type="text" variant="two" label="FirstName" />
+                        <TextFieldExplained name="name" type="text" variant="two" label="LaststName" />
+                    </div>
+                    <div className="input-field2">
+                        <TextFieldExplained name="name" type="email" variant="two" label="Emails" />
+                    </div>
+                    <div className="input-field3">
+                        <TextFieldExplained onChange={onPasswordChange} name="passcode" type="password" variant="two" label="Password" />
+                        <TextFieldExplained onChange={onPasswordChange} name="passcode" type="password" variant="two" label="Re-type Password" helperText="confirm Password" />
+
+                    </div>
+                    <button className='signup-btn'>SIGN UP</button>
+                    <div className="end">
+                        <h5>Already have an account?Sign in now</h5>
+                    </div>
+                </form>
+            </div>
         </div>
-    <div className="input-field">
-    <TextFieldExplained  name="name" type="text" variant="two" label="New Username" /> 
 
-    <TextFieldExplained onChange={onEmailChange} name="Email" type="email" variant="two" label="Email" errorText="Invalid email" helperText="Email should contain @ sign"/>
-
-    </div>
-        <div className="input-field">
-        <TextFieldExplained onChange={onPasswordChange} name="passcode" type="password" variant="two" label="Password" helperText="characters contain @,!,? and capital letters"/>
-        <TextFieldExplained onChange={onPasswordChange} name="passcode" type="password" variant="two" label="Re-type Password" helperText="confirm Password"/>
-  
-        </div>
-        <button className='signup-btn'>SIGN UP</button>
-    </form>
-</div>
-</div>
-)
+    )
 
 
 
