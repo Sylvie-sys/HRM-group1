fb415c14d622775172268c7831d8c017a725add1
Justin Bahati <bajustone@gmail.com>
2023-01-18 14:42:14 +0200

    adding auth repository

---
+++ src/data/repository/authentication-impl.ts
@@ -0,0 +1,27 @@
+import { Authentication, LoginOptions, VerifyUserResponse } from "./authentication";
+
+export default class AuthenticationImpl implements Authentication {
+    private API_ENDPOINT = "https://web-production-3d1f.up.railway.app/api/auth";
+    async register(userData: NewUser): Promise<User> {
+        const endpoint = `${this.API_ENDPOINT}/register`;
+        const requestOptions: RequestInit = {
+            method: "POST",
+            headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userData)
+        }
+        
+        const response = await fetch(endpoint, requestOptions);
+        const resultPayload = await response.json();
+        const user = resultPayload.payload;
+        return user;
+    }
+    verify(code: string): VerifyUserResponse {
+        throw new Error("Method not implemented.");
+    }
+    login({ email: password }: LoginOptions): User {
+        throw new Error("Method not implemented.");
+    }
+}
