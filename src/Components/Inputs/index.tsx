d7d9db4ac63ee3298163cab480d45a0b58cd9ac2
UwaSandrine <107852848+UwaSandrine@users.noreply.github.com>
2023-01-20 15:26:08 +0200

    add employee

--- src/Components/Inputs/index.tsx
+++ src/Components/Inputs/index.tsx
@@ -3,21 +3,32 @@ import "./index.css"
 
 const Input =() =>{
 return(
-
-     <div>
-        <input type="text" placeholder="firstname" />
-        <input type="text" placeholder="lastname" />
-        <br/>
-        <input type="email" placeholder="email" />
-        <input type="number" placeholder="Phonenumber" />
-        <br/>
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirpassword" />
-        <br/>
-        <input type="date" placeholder="dateofbirth" />
-        <input type="" />
-        <input type="image" placeholder="personalpic" />
-     </div>
+    <form id="form-1">
+    <div>
+        <button className="right-button"> Upload photo</button>
+        <button className="left-button"> Edit</button>
+    </div>
+    <div className="inputs">
+        <div className="inputs1">
+            <input type="text" placeholder="Employee name" />
+            <input type="text" placeholder="Employee ID" /> 
+        </div>
+            <div className="inputs2">
+                <input type="email" placeholder="Employee Email" />
+                <input type="text" placeholder="Employee phone number" /> 
+            </div>
+            <div className="inputs3">
+                <input type="text" placeholder="Employee education level" />
+                <input type="text" placeholder="Sex" /> 
+            </div>
+            <div className="inputs3">
+                    <input type="text" placeholder="Employee Skills" />
+                    <input type="text" placeholder="Address" /> 
+            </div>
+            </div>
+                    <button className="uploadbutton"> Upload Document</button>
+                    <button className="last-button">save changes</button> 
+    </form>
 )
 
 }
